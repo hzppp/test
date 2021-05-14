aefd97da42bee7e71df81c7ee9c206abdf864871
DESKTOP-83FLJUU\88 <hehailong@pconline.com.cn>
2021-05-14 17:37:15 +0800

    获取优惠pages

---
+++ components/apop/aPop.vue
@@ -0,0 +1,23 @@
+<template>
+    <view class="a-pop">
+        1111111111111111
+    </view>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+.a-pop {
+    position: fixed;
+    width: 100vw;
+    height: 100vh;
+    background-color: #333333;
+    opacity: .5;
+    top: 0;
+    left: 0;
+}
+</style>
