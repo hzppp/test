a83de5276da9d1c7a5d6c261a963a61fdd6270a7
yuxuekai <yuxuekai@pconline.com.cn>
2021-05-18 11:33:56 +0800

    1

---
+++ pages/chooseCity.vue
@@ -0,0 +1,36 @@
+<template>
+	<view class="choose-city">
+		city
+	</view>
+</template>
+
+<script>
+import api from '@/public/api/index'
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		onLoad() {
+			this.getAllCityList()
+		},
+		methods: {
+			async getAllCityList() {
+				try{
+					const res = await api.fetchAllCityList({hasHot:0})
+					console.log('allcity==>',res)
+				}catch(e){
+					console.error(e)
+				}
+				
+			}
+		},
+	}
+</script>
+
+<style>
+.choose-city {
+	
+}
+</style>
