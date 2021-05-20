53c42d546be74aeca1474edb72298f02c2afcd06
liangzhicong <liangzhicong1@pconline.com.cn>
2021-05-20 15:53:31 +0800

    福利社修改优惠券和活动的加载条数

--- pages/welfareActivity.vue
+++ pages/welfareActivity.vue
@@ -199,7 +199,7 @@ export default {
         let cityId = this.crtCityItem.id || currentLocation.cityData.cityId
         let {
           rows
-        } = await api.getactivity(cityId, 3, this.activityListPageNumber)
+        } = await api.getactivity(cityId, 5, this.activityListPageNumber)
         this.activityListPageNumber++
         if (rows.length > 0) {
           this.isLoadGetActivity = true
@@ -229,16 +229,16 @@ export default {
       let currentLocation = app.globalData.currentLocation
       let cityId = this.crtCityItem.id || currentLocation.cityData.cityId
 
-      let data = await api.getWelfare(cityId, 3, this.welfarePageNumber)
+      let data = await api.getWelfare(cityId, 2, this.welfarePageNumber)
       this.welfarePageNumber++
       let rows = data.rows ? data.rows : []
-      let total = Math.ceil(data.total / 3)
+      let total = Math.ceil(data.total / 2)
       let a = [...this.welfareList, ...rows]
       this.welfareList = a
       console.log('welfareList', this.welfareList)
       if(this.welfareList.length == 0){return;}
       this.$nextTick(function(){
-        if (this.welfarePageNumber > total || rows.length < 3) {
+        if (this.welfarePageNumber > total || rows.length < 2) {
           // this.$invoke('coupon-list', 'morebtnHide')
           console.log('couponlist=================',this.$refs.couponlist)
           this.$refs.couponlist.morebtnHide()
