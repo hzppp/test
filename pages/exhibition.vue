62da88dd77eb8dc79b4c6f9816742539f2615082
陈新垲 <chenxinkai@pconline.com.cn>
2021-05-22 11:59:49 +0800

    update

--- pages/exhibition.vue
+++ pages/exhibition.vue
@@ -1,6 +1,7 @@
 <template>
   <view>
 		<web-view src="https://cdc.pcauto.com.cn/vue/hall/a/"></web-view>
+		<!-- <web-view src="http://localhost:8080/"></web-view> -->
 <!--    <button v-if="!haveUserInfoAuth" class="getUserInfo_name_info_mask_body" @tap="getWxUserInfoAuth"></button>-->
     <!-- <viewTabBar :current="2"></viewTabBar> -->
   </view>
