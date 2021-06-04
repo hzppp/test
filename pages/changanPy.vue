22b40765d0b28ea17fbf64936341815a94600413
雨尘 <719967870@qq.com>
2021-06-04 09:27:57 +0800

    发布

--- pages/changanPy.vue
+++ pages/changanPy.vue
@@ -1,6 +1,6 @@
 <template>
 	<view>
-		<web-view src="https://www1.pcauto.com.cn/zt/gz20210530/changan/xcx/changanPy.html"></web-view>
+		<web-view src="https://ccar.pcauto.com.cn/zt/gz20210530/changan/xcx/changanPy.html"></web-view>
 	</view>
 </template>
 
