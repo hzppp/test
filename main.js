0933d78c1ba63992c490e3a1c0e750126201f4f3
李雁 <liyan1907@pconline.com.cn>
2021-12-10 15:25:50 +0800

    feat：长安埋点sdk引入

--- main.js
+++ main.js
@@ -14,7 +14,14 @@ Vue.prototype.$EventBus = new Vue()
 Vue.config.productionTip = false
 
 App.mpType = 'app'
-
+var gdp = require("./units/gio-minp").default;
+gdp('init', 'ac8e9fff9218d0b4', 'a8baef5711ad838e', 'wxb36fb5205e5afb36', {
+    version: '小程序版本',
+    host: 'https://cbd-api.changan.com.cn:9090',
+    vue: Vue,
+    debug: true
+});
+gdp('track', 'YCZ_openApplet');
 const app = new Vue({
 	store,
     ...App
