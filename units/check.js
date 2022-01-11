c328ce8ceaddd2b4837f2cb1c2a132a893c8b7f5
雨尘 <719967870@qq.com>
2022-01-11 12:29:41 +0800

    feat:接入微信客服

--- units/check.js
+++ units/check.js
@@ -20,3 +20,34 @@ export const trim = function(str) {
 	//去除两边空格
 	return str.replace(/(^\s*)|(\s*$)/g, "");
 };
+// >= 0 成立
+export const checkVersion = function(v2) {
+	var v1 = wx.getSystemInfoSync().SDKVersion
+    console.log('本地版本',v1)
+	v1 = v1.split('.')
+	v2 = v2.split('.')
+	const len = Math.max(v1.length, v2.length)
+
+	while (v1.length < len) {
+		v1.push('0')
+	}
+	while (v2.length < len) {
+		v2.push('0')
+	}
+
+	for (let i = 0; i < len; i++) {
+		const num1 = parseInt(v1[i])
+		const num2 = parseInt(v2[i])
+
+		if (num1 > num2) {
+			return 1
+		} else if (num1 < num2) {
+			return -1
+		}
+	}
+
+	return 0
+
+
+
+};
