7d393dea26aecea862b6b8642210b7bb5c03732a
huangzepeng <huangzepeng@pconline.com.cn>
2022-02-15 10:17:02 +0800

    新增到店登记页面

---
+++ .history/App_20220211091128.vue
@@ -0,0 +1,140 @@
+<script>
+	import '@/public/ald/ald-stat.js'
+	export default {
+		globalData: {
+			haveUserInfoAuth: false, //是否缺少授权 false:已经授权
+			csbHost: "https://price.pcauto.com.cn", //基础信息
+			whoisHost: "https://whois.pconline.com.cn", //位置信息
+			magearHost: "https://magear.pcauto.com.cn",
+			host: "https://testpocket.pcauto.com.cn",
+			panorama: "https://panorama.pcauto.com.cn", //vr看车图片
+			//panorama:"http://t-panorama.pcauto.com.cn",
+			testDriveX: 400,
+			testDriveY: 465,
+			adDealerId:'',  // 留资来源id
+			currentLocation: { //当前位置
+				wxPosition: { // 微信定位的坐标位置
+					wsq: true,
+					latitude: 29.57,
+					longitude: 106.5,
+					provider: 'default',
+				},
+				cityData: { // 当前定位的位置
+					"city": "重庆市",
+					"cityCode": "500000",
+					"error": "",
+					"pro": "重庆市",
+					"proCode": "500000",
+					"region": "江北区",
+					"regionCode": "500105"
+				},
+				selectedCityData: { // 当前选择的位置
+					"isChange": false, // 是否已手动修改
+					"pro": "重庆市",
+					"city": "重庆市",
+				},
+			},
+			userInfo: null,
+			UPCHost: "https://upc.pcauto.com.cn/upload_quick.jsp?application=autopocket",
+			wxUserInfo: null,
+			phone: '', //  授权的手机号码
+			provinceList: [], // 省市列表
+			hasAuthorized:false,
+			avtivityEndTime:""
+		},
+		onLaunch: function() {
+			// #ifndef MP-TOUTIAO
+			uni.hideTabBar({
+				animation: false
+			})
+			// #endif
+			console.log('App Launch')
+			if (uni.canIUse('loadFontFace')) {
+				let _this = this
+				console.log("支持自定义字体");
+				uni.loadFontFace({
+					global: true,
+					family: 'Ping Fang',
+					source: 'url("https://www1.pcauto.com.cn/zt/gz20210530/changan/xcx/font/ChangAnunitype-Regular.TTF")',
+					success(res) {
+						_this.fontLoaded = true
+						console.log("字体加载成功") //  loaded
+					},
+					fail(res) {
+						console.log("字体加载失败") //  error
+					},
+					complete(res) {
+						console.log("字体加载完成1");
+					}
+				});
+
+			} else {
+				console.log('不支持自定义字体')
+			}
+
+		},
+		onShow: function() {
+			console.log('App Show')
+			
+		},
+		onHide: function() {
+			console.log('App Hide')
+		},
+		methods: {}
+	}
+</script>
+
+<style lang="less">
+	/*每个页面公共css */
+	body {
+		font-family: Ping Fang;
+	}
+
+	// @font-face {
+	//   font-family: ChangAnunitype;
+	//   src: url("~@/static/font/ChangAnunitype-Regular.TTF") format("truetype");
+	// }
+	.autoFont {
+		font-family: Ping Fang;
+	}
+
+	.container {
+		height: 100%;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: space-between;
+		box-sizing: border-box;
+	}
+
+	page {
+		word-break: break-all;
+	}
+
+	button {
+		border: 0;
+		padding: 0;
+		margin: 0;
+		border-radius: 0;
+	}
+
+	button:after {
+		border-radius: 0;
+		border: 0;
+	}
+
+	button[plain] {
+		border: 1px;
+		border-radius: 0;
+	}
+
+	.getUserInfo_name_info_mask_body {
+		position: fixed;
+		left: 0;
+		top: 0;
+		width: 100%;
+		height: 100%;
+		z-index: 9999;
+		background: none;
+	}
+</style>
---
+++ pages/register.vue
@@ -0,0 +1,213 @@
+<template>
+	<view class="register">
+		<view class="introduction">
+			<text>请选择心仪的车型</text>
+		</view>
+		<scroll-view scroll-y="true" style="height: 825rpx;">
+				<view class="content">
+					<view v-for="i in 8" class="carBox" @click="chooseCar(i)" :class="{'active': i == activeCar}">
+						<view class="carImg">
+							<image src="../static/images/car1.png" mode="" class="img"></image>
+						</view>
+						<!-- <view class="carName"> -->
+						<text class="carName">UNI-T</text>
+						<!-- </view> -->
+					</view>
+				</view>
+		</scroll-view>
+		<view class="bottom">
+			<view class="tel">
+				<view class="tel-label">
+					手机号
+				</view>
+				<!-- <view class="tel-input"> -->
+					<input class="tel-input" type="text" value="" placeholder="点击授权微信手机号" />
+				<!-- </view> -->
+			</view>
+			<!-- <view class="code">
+				<view class="code-label">
+					验证码
+				</view>
+				<view class="code-input">
+					<input type="text" value="" placeholder="请输入验证码" />
+				</view>
+			</view> -->
+			
+			<view class="submit">
+				<view class="sub-text">
+					提交登记信息
+				</view>
+			</view>
+			
+			<view class="tongyi">
+				点击按钮即视为同意《用户隐私协议》
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				carsList : [
+					{
+						imgUrl : ''
+					}
+				],
+				activeCar : 0
+			}
+		},
+		methods: {
+			chooseCar(i){
+				console.log(i)
+				this.activeCar = i
+			}
+		}
+	}
+</script>
+
+<style lang="less">
+@import '@/static/less/public.less';
+.register{
+	position: relative;
+	height: 100vh;
+}
+
+.introduction{
+	margin-bottom: 29rpx;
+}
+
+.introduction text{
+	width: 167rpx;
+	height: 20rpx;
+	font-size: 24rpx;
+	font-family: changan uni type, changan uni type-Regular;
+	font-weight: 400;
+	text-align: left;
+	color: #999999;
+	margin-left: 4%;
+}
+
+.content{
+	display: flex;
+	flex-wrap: wrap;
+	justify-content: space-between;
+	padding: 0 32rpx;
+}
+
+.content .carBox{
+	width: 330rpx;
+	height: 298rpx;
+	border-radius: 20rpx;
+	margin-bottom: 24rpx;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	box-sizing: border-box;
+}
+
+.carImg{
+	width: 290rpx;
+	height: 193rpx;
+	margin: 20rpx;
+}
+
+.img{
+	width: 100%;
+	height: 100%;
+	border-radius: 20rpx;
+}
+
+.carName{
+	text-align: center;
+	height: 19rpx;
+	font-size: 26rpx;
+	font-family: changan uni type, changan uni type-Bold;
+	font-weight: 700;
+	text-align: center;
+	color: #333333;
+	margin-top: 2%;
+	margin-bottom: 40rpx;
+}
+
+.bottom{
+	width: 750rpx;
+	height: 310rpx;
+	box-sizing: border-box;
+	background: #ffffff;
+	// border-radius: 20px;
+	box-shadow: 0rpx -8rpx 6rpx 6rpx rgba(0,0,0,0.05); 
+	padding: 0 32rpx 0 32rpx;
+}
+
+.tel{
+	height: 114rpx;
+	display: flex;
+	align-items: center;
+	margin-top: 17rpx;
+	border-bottom: 1px solid #ebebeb;
+}
+
+
+.tel-label{
+	width: 94rpx;
+	font-size: 32rpx;
+	font-family: changan uni type, changan uni type-Bold;
+	font-weight: 700;
+	text-align: left;
+	color: #333333;
+}
+
+.tel-input{
+	width: 285rpx;
+	height: 27rpx;
+	font-size: 32rpx;
+	font-family: changan uni type, changan uni type-Regular;
+	font-weight: 400;
+	text-align: left;
+	color: #999999;
+	margin-left: 90rpx;
+}
+
+.submit{
+	width: 686rpx;
+	height: 88rpx;
+	background: #fa8845;
+	border-radius: 44rpx;
+	text-align: center;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	margin-top: 30rpx;
+}
+
+.sub-text{
+	width: 190rpx;
+	height: 26rpx;
+	font-size: 32rpx;
+	font-family: changan uni type, changan uni type-Regular;
+	font-weight: 400;
+	text-align: center;
+	color: #ffffff;
+	vertical-align: center;
+	margin-top: -11rpx;
+	// margin-top: 32rpx;
+	// margin-bottom: 30rpx;
+}
+
+.active{
+	border: 4rpx solid #fa8845;
+}
+
+.tongyi{
+	// width: 398rpx;
+	// height: 20rpx;
+	font-size: 24rpx;
+	font-family: changan uni type, changan uni type-Regular;
+	font-weight: 400;
+	text-align: center;
+	color: #999999;
+	margin: 0 auto;
+}
+</style>
